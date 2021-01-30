0d29f481d6e48ceb26769c1e38e6951aa40f7b3e
ChoiSeoungMuk <sawdonnie@icloud.com>
2021-01-31 01:28:41 +0900

    before edit function update
    edit function is working just one time not several.

---
+++ todo.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+
+    for(var name in listControl) {
+        listControl[name]();
+    }
+}
+
+var listControl = Object.create(null);
+
+listControl.generate = function() {
+
+    var button = document.getElementById("submitButton");
+    var contentDiv = document.getElementById("contentWrap");
+    var listNumber = 0;
+    var editNumber = 0;
+    button.addEventListener("click", submitClicked, false);
+    
+    function submitClicked(e){
+        //추가
+        listNumber += 1;
+        editNumber += 1;
+        var text = document.getElementById("inputValue").value;
+        var timeDiv = elt("div",{class: "time"},"시간");
+        var subtitle = elt("p",null,text);
+        var titldDiv = elt("div",{class: "subtitle"},subtitle);
+        var editbtn = elt("input", {type: "button", id:"##"+listNumber,value: "수정"});
+        var removebtn = elt("input", {type: "button",id: "#"+listNumber ,value: "삭제"});
+        var buttonDiv = elt("div", {class:"changeButton"},editbtn,removebtn);
+        var listDiv = elt("div",{class: "list", id:listNumber},timeDiv,titldDiv,buttonDiv);
+
+        contentDiv.appendChild(listDiv);
+        
+        var promise = new Promise(function(submitClicked, reject){
+            submitClicked()
+        });
+        //수정
+        promise.then(function() {
+
+            listControl.remove = function remove() {
+                var eachBtn = document.getElementById(removebtn.id);
+                eachBtn.addEventListener("click",function(){
+                    listDiv.remove();
+                },false);
+            }();
+
+            listControl.edit = function() {
+                var editBtn = document.getElementById(editbtn.id);
+                editBtn.addEventListener("click",function(){
+                    var editText = prompt("입력하세요");
+                    var newSubTitle = elt("p",null,editText);
+                    console.log(subtitle);
+                    
+                    subtitle.parentNode.replaceChild(newSubTitle, subtitle);
+                },false);
+            }();
+
+        });
+
+        //입력
+
+    }
+    
+};
+
+
